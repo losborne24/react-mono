95ff8fa4397095f67a6f02f0af84d19a62dccef0
Leith Osborne <osborneleith@gmail.com>
2026-07-12 18:56:50 +0100

    mosaify

---
+++ apps/mosaify/src/app/app.tsx
@@ -0,0 +1,11 @@
+import './app.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="mosaify" />
+    </div>
+  );
+}
+export default App;
